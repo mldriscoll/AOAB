d73bd19aeea1cf8e192290129e58a7cb457c0079
mldriscoll <53544431+mldriscoll@users.noreply.github.com>
2025-01-22 23:01:39 +0000

    AOAB Fanbook 5

--- AOABO/Chapters/Chapter.cs
+++ AOABO/Chapters/Chapter.cs
@@ -42,7 +42,7 @@ namespace AOABO.Chapters
                 "0301" or "0302" or "0303" or "0304" or "0305" => $"{Configuration.FolderNames["PartThree"]}\\03-Story",
                 "0401" or "0402" or "0403" or "0404" or "0405" or "0406" or "0407" or "0408" or "0409" => $"{Configuration.FolderNames["PartFour"]}\\03-Story",
                 "0501" or "0502" or "0503" or "0504" or "0505" or "0506" or "0507" or "0508" or "0509" or "0510" or "0511" or "0512" => $"{Configuration.FolderNames["PartFive"]}\\03-Story",
-                "FB1" or "FB2" or "FB3" or "FB4" => $"11-Fanbooks",
+                "FB1" or "FB2" or "FB3" or "FB4" or "FB5" => $"11-Fanbooks",
                 _ => throw new Exception($"GetPartSubFolder - {ChapterName}"),
             };
         }
@@ -50,7 +50,7 @@ namespace AOABO.Chapters
         {
             return Volume switch
             {
-                "FB1" or "FB2" or "FB3" or "FB4" => $"06-Fanbooks",
+                "FB1" or "FB2" or "FB3" or "FB4" or "FB5" => $"06-Fanbooks",
                 _ => $"03-Story\\[Year]\\{GetSeason()}"
             };
         }
@@ -64,7 +64,7 @@ namespace AOABO.Chapters
                 "0301" or "0302" or "0303" or "0304" or "0305" => $"{Configuration.FolderNames["PartThree"]}\\{Volume}-{getVolumeName()}",
                 "0401" or "0402" or "0403" or "0404" or "0405" or "0406" or "0407" or "0408" or "0409" => $"{Configuration.FolderNames["PartFour"]}\\{Volume}-{getVolumeName()}",
                 "0501" or "0502" or "0503" or "0504" or "0505" or "0506" or "0507" or "0508" or "0509" or "0510" or "0511" or "0512" => $"{Configuration.FolderNames["PartFive"]}\\{Volume}-{getVolumeName()}",
-                "FB1" or "FB2" or "FB3" or "FB4" => $"06-Fanbooks",
+                "FB1" or "FB2" or "FB3" or "FB4" or "FB5" => $"06-Fanbooks",
                 _ => throw new Exception($"GetVolumeSubFolder - {ChapterName}")
             };
         }
@@ -93,7 +93,7 @@ namespace AOABO.Chapters
         {
             return Volume switch
             {
-                "FB1" or "FB2" or "FB3" => $"06-Fanbooks",
+                "FB1" or "FB2" or "FB3" or "FB4" or "FB5" => $"06-Fanbooks",
                 _ => $"03-Story"
             };
         }
