6ea5f5838f1503a653eb2d30e18ebc6e0b20c4a2
aoab-contributor <124131985+aoab-contributor@users.noreply.github.com>
2025-05-12 16:02:54 -0400

    * Hide password entry on Console at time of typing  * Separate GetUsername and GetPassword functions in anticipation of using them independently  * Created functions for persisting/retrieving usename and password - ensures single source of truth for Account.txt handling  * Modifide FromConsole() to only persist password if Login object successfully created - this mirrors the behavior of the GUI
    Signed-off-by: aoab-contributor <124131985+aoab-contributor@users.noreply.github.com>

--- Core/Login.cs
+++ Core/Login.cs
@@ -23,24 +23,83 @@ namespace Core
             byte[] plaintextBuffer = ProtectedData.Unprotect(ciphertextBuffer, null, DataProtectionScope.CurrentUser);
             return Encoding.ASCII.GetString(plaintextBuffer);
         }
-        
+
+        public static string ConsoleGetUsername()
+        {
+            Console.WriteLine("Please enter your j-novel club username");
+            return Console.ReadLine()!;
+
+        }
+
+        public static string ConsoleGetPassword()
+        {
+            Console.WriteLine("Please enter your j-novel club password");
+
+            // Copied from https://stackoverflow.com/questions/3404421/password-masking-console-application
+            var pass = string.Empty;
+            ConsoleKey key;
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && pass.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    pass = pass[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    pass += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+
+            return pass;
+        }
+
+        public static async Task PersistLoginInfo(string username, string password)
+        {
+            await File.WriteAllTextAsync("Account.txt", $"{username}\r\n{Convert.ToBase64String(ToCiphertext(password))}");
+        }
+
+        public static Tuple<string, string>? RetrieveLoginInfo()
+        {
+            if (!File.Exists("Account.txt"))
+            {
+                return null;
+            }
+            else
+            {
+                var text = File.ReadAllText("Account.txt");
+                var split = text.Split("\r\n");
+
+                if (split.Length != 2)
+                {
+                    return null;
+                }
+                else
+                {
+                    return new(split[0], FromCiphertext(Convert.FromBase64String(split[1])));
+                }
+            }
+        }
+
         public static async Task<Login?> FromConsole(HttpClient client)
         {
             Console.Clear();
             Console.WriteLine("Creating a user account file");
             Console.WriteLine();
-            Console.WriteLine("Please enter your j-novel club username");
-            var un = Console.ReadLine()!;
+            string username = ConsoleGetUsername();
+            string password = ConsoleGetPassword();
 
-            Console.WriteLine("Please enter your j-novel club password");
-            var pass = Console.ReadLine()!;
+            Login? login = await CreateLogin(username, password, client);
+            if (login != null)
+            {
+                await PersistLoginInfo(username, password);
+            }
 
-            string username = un!;
-            string password = pass!;
-
-            File.WriteAllText("Account.txt", $"{username}\r\n{Convert.ToBase64String(ToCiphertext(password))}");
-            
-            return await CreateLogin(un!, ct, client);
+            return login;
         }
 
         public static async Task<Login?> FromUI(HttpClient client, string username, string password)
@@ -48,7 +107,7 @@ namespace Core
             var login = await CreateLogin(username, password, client);
             if (login != null)
             {
-                File.WriteAllText("Account.txt", $"{username}\r\n{Convert.ToBase64String(ToCiphertext(password))}");
+                await PersistLoginInfo(username, password);
             }
 
             return login;
@@ -56,19 +115,15 @@ namespace Core
 
         public static async Task<Login?> FromFile(HttpClient client)
         {
-            if (!File.Exists("Account.txt"))
+            var loginInfo = RetrieveLoginInfo();
+            if (loginInfo != null)
+            {
+                return await CreateLogin(loginInfo.Item1, loginInfo.Item2, client);
+            }
+            else
             {
                 return null;
             }
-
-            var text = File.ReadAllText("Account.txt");
-            var split = text.Split("\r\n");
-            if (split.Length == 2)
-            {
-                return await CreateLogin(split[0], FromCiphertext(Convert.FromBase64String(split[1])), client);
-            }
-
-            return null;
         }
 
         private static async Task<Login?> CreateLogin(string username, string plaintextPassword, HttpClient client)
